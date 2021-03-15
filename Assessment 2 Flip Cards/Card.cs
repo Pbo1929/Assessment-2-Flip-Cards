3596e79d0cea362e7ee8f607750b01f790d3d12e
Prem Worapon Sumsai <lulztit1929@gmail.com>
2021-03-15 12:14:38 +0700

    Add project files.

---
+++ Assessment 2 Flip Cards/Card.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assessment_2_Flip_Cards
+{
+    class Card
+    {
+        private string CardName;
+        private string CardDefinition;
+        private int CardCount;
+        private bool Flip;
+
+        public Card(string CardName, string CardDefinition)
+        {
+            this.CardName = CardName;
+            this.CardDefinition = CardDefinition;
+            Flip = false;
+        }
+
+        public int GetCount()
+        {
+            return CardCount;
+        }
+
+        public string Flipcard()
+        {
+            if (Flip == false)
+            {
+                Flip = true;
+                return CardDefinition;
+            }
+            else
+            {
+                Flip = false;
+                return CardName;
+            }
+        }
+    }
+}
---
+++ Assessment 2 Flip Cards/Deck.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assessment_2_Flip_Cards
+{
+    class Deck
+    {
+        Card[] Cards;
+        private string FileName;
+    }
+
+    public void ShuffleDeck()
+    {
+        for(int i = 0; i < 
+    }
+}
