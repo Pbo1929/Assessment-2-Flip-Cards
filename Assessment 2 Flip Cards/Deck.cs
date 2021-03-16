15e7e3bf7cfa47d95328266e71cda615094b0e24
Prem Worapon Sumsai <lulztit1929@gmail.com>
2021-03-16 15:01:29 +0700

    Shuffle Card, Load Deck.

--- Assessment 2 Flip Cards/Deck.cs
+++ Assessment 2 Flip Cards/Deck.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,12 +9,51 @@ namespace Assessment_2_Flip_Cards
 {
     class Deck
     {
-        Card[] Cards;
         private string FileName;
-    }
+        Card[] Cards;
 
-    public void ShuffleDeck()
-    {
-        for(int i = 0; i < 
+        public Deck(string FileName)
+        {
+            this.FileName = FileName;
+            Cards = new Card[4];
+        }
+
+        public string GetFileName()
+        {
+            return FileName;
+        }
+
+        public void LoadDeck(string FileName)
+        {
+            string line;
+            int count = 0;
+            StreamReader FileReader = new StreamReader(FileName);
+            while ((line = FileReader.ReadLine()) != null)
+            {
+                string[] cells = line.Split(',');
+                string word = cells[0];
+                string definition = cells[1];
+                Cards[count] = new Card(word, definition);
+                count++;
+            }
+        }
+
+        public void ShuffleCard()
+        {
+            Random rnd = new Random();
+            int count = 0;
+
+            for(int i = 0; i < Cards.Length; i++)
+            {
+                int num = rnd.Next(4);
+                Cards[count] = Cards[num];
+                count++;
+            }
+        }
+
+        public void RandomCard()
+        {
+            
+        }
     }
 }
